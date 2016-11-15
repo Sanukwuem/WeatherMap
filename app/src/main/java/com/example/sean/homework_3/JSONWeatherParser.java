09011dfe0b0c314c3a7e02e273f3a010dad2714f
Sean Anukwuem <anukwus1@tcnj.edu>
2016-11-14 20:00:07 -0500

    JSON Parser complete

---
+++ app/src/main/java/com/example/sean/homework_3/JSONWeatherParser.java
@@ -0,0 +1,53 @@
+package com.example.sean.homework_3;
+
+/**
+ * Created by Sean on 11/14/2016.
+ */
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.sean.homework_3.model.Location;
+import com.example.sean.homework_3.model.Weather;
+
+public class JSONWeatherParser {
+
+    public static void getWeather(String data) throws JSONException {
+        Weather weather = new Weather();
+
+        // We create out JSONObject from the data
+        JSONObject jObj = new JSONObject(data);
+
+        // We start extracting the info
+        Location loc = new Location();
+
+        JSONObject coordObj = getObject("coord", jObj);
+        loc.setLatitude(getFloat("lat", coordObj));
+        loc.setLongitude(getFloat("lon", coordObj));
+
+        JSONObject sysObj = getObject("sys", jObj);
+        loc.setCountry(getString("country", sysObj));
+        loc.setSunrise(getInt("sunrise", sysObj));
+        loc.setSunset(getInt("sunset", sysObj));
+        loc.setCity(getString("name", jObj));
+        weather.location = loc;
+    }
+
+    private static JSONObject getObject(String tagName, JSONObject jObj) throws JSONException {
+        JSONObject subObj = jObj.getJSONObject(tagName);
+        return subObj;
+    }
+
+    private static String getString(String tagName, JSONObject jObj) throws JSONException {
+        return jObj.getString(tagName);
+    }
+
+    private static float getFloat(String tagName, JSONObject jObj) throws JSONException {
+        return (float) jObj.getDouble(tagName);
+    }
+
+    private static int getInt(String tagName, JSONObject jObj) throws JSONException {
+        return jObj.getInt(tagName);
+    }
+}
